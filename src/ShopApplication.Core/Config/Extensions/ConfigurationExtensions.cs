203a7fd35d6ea2a39cbd3383859cca88be627e95
Dimanopoulos Michalis <E81545@NBG.GR>
2021-01-23 19:07:33 +0200

    Add App Configuration

---
+++ src/ShopApplication.Core/Config/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ShopApplication.Core.Config.Extensions
+{
+    public static class ConfigurationExtensions
+    {
+        public static AppConfig ReadAppConfiguration(this IConfiguration @this)
+        {
+            var connectionString = @this.GetConnectionString("ShopDb");
+
+            AppConfig appConfig = new AppConfig();
+            appConfig.DbConnectionString = connectionString;
+            return appConfig;
+        }
+    }
+}
